6ad9e4bdf26606f8383c2d829845148a421e376e
Ian Frye <ian_frye@outlook.com>
2023-10-14 10:16:09 -0400

    fixing gitignore

---
+++ Starwars Choose your Own Adventure Python/main.py
@@ -0,0 +1,72 @@
+ from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from langchain.memory import CassandraChatMessageHistory, ConversationBufferMemory
+from langchain.llms import OpenAI
+from langchain import LLMChain, PromptTemplate
+import json
+
+cloud_config= {
+  'secure_connect_bundle': 'secure-connect-open-api-playground.zip'
+}
+
+with open("open_api_playground-token.json") as f:
+    secrets = json.load(f)
+
+CLIENT_ID = secrets["clientId"]
+CLIENT_SECRET = secrets["secret"]
+ASTRA_DB_KEYSPACE = "database"
+OPENAI_API_KEY = "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"
+
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+message_history = CassandraChatMessageHistory(
+    session_id="anything",
+    session=session,
+    keyspace=ASTRA_DB_KEYSPACE,
+    ttl_seconds=3600
+)
+
+message_history.clear()
+
+cass_buff_memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    chat_memory=message_history
+)
+
+template = """\n
+You are a young Jedi Knight, freshly trained in the ways of the Force. The galaxy is in turmoil as the Sith Empire rises to power once more, threatening to bring darkness and chaos to the galaxy. As one of the last hopes for peace, you must embark on a perilous journey. Your mission is to:
+
+Here are some rules to follow:
+1. Start by asking the player to choose some kind of weapons that will be used later in the game
+2. Have a few paths that lead to success
+3. Have some paths that lead to death. If the user dies generate a response that explains the death and ends in the text: "The End.", I will search for this text to end the game
+
+
+Here is the chat history, use this to understand what to say next: {chat_history}
+Human: {human_input}
+AI:"""
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "human_input"],
+    template=template
+)
+
+llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+llm_chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    memory=cass_buff_memory
+)
+
+choice = "start"
+
+while True:
+    response = llm_chain.predict(human_input=choice)
+    print(response.strip())
+
+    if "The End." in response:
+        break
+
+    choice = input("Your reply: ")
